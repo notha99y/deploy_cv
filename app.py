d6e58dced05053e0946a436b07c3cc8bc721eb06
notha99y <renjie.tan91@gmail.com>
2020-06-03 16:19:44 +0800

    flask and detr

---
+++ app.py
@@ -0,0 +1,46 @@
+import numpy as np
+from flask import Flask, jsonify, render_template, request
+from PIL import Image
+
+from detr import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        input_img = request.files["input-img"]
+        img = Image.open(input_img.stream)
+        print(img.size)
+        detected_classes, confidences, bboxes = detect(img, model, transform)
+        detections_info = []
+        for i, _class in enumerate(detected_classes):
+            detections_info.append(
+                {
+                    "class": _class,
+                    "confidence": confidences[i],
+                    "bbox": bboxes[i],
+                }
+            )
+
+        response = {
+            "success": True,
+            "detection_result": detections_info,
+        }
+    except Exception as e:
+        print(e)
+        print("PREDICTION ERROR")
+        response = {"success": False}
+
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001", debug=True)
---
+++ detr.py
@@ -0,0 +1,69 @@
+import torch
+import torchvision.transforms as T
+
+torch.set_grad_enabled(False)
+
+model = torch.hub.load('facebookresearch/detr', 'detr_resnet101', pretrained=True)
+model.eval()
+
+# COCO classes
+CLASSES = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
+# standard PyTorch mean-std input image normalization
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def detect(im, model, transform):
+    with torch.no_grad():
+        # mean-std normalize the input image (batch-size: 1)
+        img = transform(im).unsqueeze(0)
+
+        # propagate through the model
+        outputs = model(img)
+
+        # keep only predictions with 0.7+ confidence
+        probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+        keep = probas.max(-1).values > 0.7
+
+        # convert boxes from [0; 1] to image scales
+        bboxes_keep = outputs['pred_boxes'][0, keep]
+        # bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
+        scores = probas[keep]
+
+        detected_classes = [CLASSES[score.argmax()] for score in scores]
+        confidences = [score.max().item() for score in scores]
+        bboxes = [bbox.tolist() for bbox in bboxes_keep]
+    return detected_classes, confidences, bboxes
+
+if __name__ == "__main__":
+    pass
